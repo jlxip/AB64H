f8bff1f74e6a274144eb02040f5a6dd9c219d4c2
JlXip <jlxip.mc@gmail.com>
2016-09-30 22:07:36 +0200

    Button added

--- AB64H.js
+++ AB64H.js
@@ -2,7 +2,7 @@
 // @name AB64H
 // @description Auto BASE64 Hangouts
 // @namespace AB64H
-// @version 0.1.1
+// @version 0.1.2
 // @include https://mail.google.com/*
 
 // @require http://code.jquery.com/jquery-git.min.js
@@ -14,37 +14,43 @@
 // @supportURL https://gitlab.com/jlxip/AB64H
 // ==/UserScript==
 
-var globalkey=prompt("PASSWORD OF COMMUNICATION");	// Pedimos la clave de cifrado para la comunicación
-var md5key=hex_md5(globalkey);	// Hasheamos la clave en md5 para obtener siempre una clave de 256 bits
-var key=init(md5key);	// Variable con la llave
-var msginput = $('div[role="textbox"]');	// Entrada de mensaje
+function run() {
+	var globalkey=prompt("PASSWORD OF COMMUNICATION");	// Pedimos la clave de cifrado para la comunicación
+	var md5key=hex_md5(globalkey);	// Hasheamos la clave en md5 para obtener siempre una clave de 256 bits
+	var key=init(md5key);	// Variable con la llave
+	var msginput = $('div[role="textbox"]');	// Entrada de mensaje
 
-// LIMPIEZA DE VARIABLES QUE PUEDEN SER EXPUESTAS (seguridad)
-globalkey=null;
-md5key=null;
+	// LIMPIEZA DE VARIABLES QUE PUEDEN SER EXPUESTAS (seguridad)
+	globalkey=null;
+	md5key=null;
 
-msginput.keypress(function(evt) {	// Función para captar las pulsaciones de teclas en dicho campo
-    evt = evt || window.event;
-    var charCode = evt.keyCode || evt.which;
-    if(charCode==170) {	// Si la tecla pulsada es ª...
-    	var oneChanged = false;	// Variable booleana para saber si se ha descifrado al menos un mensaje
-    	$('span[dir="ltr"]').each(function() {	// Por cada etiqueta "span" con el campo dir="ltr"...
-    		if($(this).html().substr(0, 5)=="AES: ") {	// Si los primeros 4 bytes son "AES: "...
-    			var bencoded=$(this).html().substr(5, $(this).html().length);	// Obtenemos lo que queda después de "AES: "
-    			var encoded=atob(bencoded);	// Lo decodificamos de BASE64 para obtener los bytes limpios
-    			var decoded=decryptLongString(encoded, key);	// Decodificamos el mensaje
-    			$(this).html(decoded);	// Y lo cambiamos en el documento HTML
-    			oneChanged = true;	// Cambiamos el valor de la variable booleana para saber que al menos se ha descifrado un mensaje
-    		}
-    	});
+	msginput.keypress(function(evt) {	// Función para captar las pulsaciones de teclas en dicho campo
+	    evt = evt || window.event;
+	    var charCode = evt.keyCode || evt.which;
+	    if(charCode==170) {	// Si la tecla pulsada es ª...
+	    	var oneChanged = false;	// Variable booleana para saber si se ha descifrado al menos un mensaje
+	    	$('span[dir="ltr"]').each(function() {	// Por cada etiqueta "span" con el campo dir="ltr"...
+	    		if($(this).html().substr(0, 5)=="AES: ") {	// Si los primeros 4 bytes son "AES: "...
+	    			var bencoded=$(this).html().substr(5, $(this).html().length);	// Obtenemos lo que queda después de "AES: "
+	    			var encoded=atob(bencoded);	// Lo decodificamos de BASE64 para obtener los bytes limpios
+	    			var decoded=decryptLongString(encoded, key);	// Decodificamos el mensaje
+	    			$(this).html(decoded);	// Y lo cambiamos en el documento HTML
+	    			oneChanged = true;	// Cambiamos el valor de la variable booleana para saber que al menos se ha descifrado un mensaje
+	    		}
+	    	});
 
-    	if(oneChanged == false) {	// Si no se ha cambiado ningún mensaje (porque se quiera cifrar texto)...
-    		var decoded=msginput.text();	// Variable con el mensaje en texto plano
-	    	var encrypted=encryptLongString(decoded, key);	// Ciframos el mensaje
-	    	var bencrypted=btoa(encrypted);	// Convertimos el mensaje cifrado en imprimible cifrándolo en BASE64
-	    	msginput.text("AES: "+bencrypted);	// Cambiamos el mensaje a enviar por el mensaje cifrado con el prefijo "AES: "
-    	}
+	    	if(oneChanged == false) {	// Si no se ha cambiado ningún mensaje (porque se quiera cifrar texto)...
+	    		var decoded=msginput.text();	// Variable con el mensaje en texto plano
+		    	var encrypted=encryptLongString(decoded, key);	// Ciframos el mensaje
+		    	var bencrypted=btoa(encrypted);	// Convertimos el mensaje cifrado en imprimible cifrándolo en BASE64
+		    	msginput.text("AES: "+bencrypted);	// Cambiamos el mensaje a enviar por el mensaje cifrado con el prefijo "AES: "
+	    	}
 
-    	return false;	// Devolvemos "false" para que no se escriba el carácter ª
-    }
-});
+	    	return false;	// Devolvemos "false" para que no se escriba el carácter ª
+	    }
+	});
+}
+
+var run_button = document.createElement("BUTTON");	// Botón para lanzar el script
+run_button.appendChild(document.createTextNode("RUN AB64H"));	// Establecemos el texto
+document.body.appendChild(run_button);	// Y lo mostramos TODO: MEJORAR
