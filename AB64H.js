d5e082b91e0ba1836d75c003785d9d333c12ef06
JlXip <jlxip.mc@gmail.com>
2016-10-03 22:56:04 +0200

    Added a return when password is provided
    For don't letting a blank message to crypt

--- AB64H.js
+++ AB64H.js
@@ -3,7 +3,7 @@
 // @description Auto BASE64 Hangouts
 // @author JlXip
 // @namespace AB64H
-// @version 1.0.0-RC1
+// @version 1.0.0-RC2
 // @include https://hangouts.google.com/webchat/*
 
 // @require http://code.jquery.com/jquery-git.min.js
@@ -17,48 +17,50 @@
 // ==/UserScript==
 
 
-var msginput = null;	// Campo de entrada del mensaje
-var key = null;	// Clave (AES-256) para cifrar las comunicaciones
+var msginput = null;    // Campo de entrada del mensaje
+var key = null; // Clave (AES-256) para cifrar las comunicaciones
 
 
-function AB64H_keypress(evt) {	// Función que se ejecutará cada vez que se pulse una tecla
+function AB64H_keypress(evt) {  // Función que se ejecutará cada vez que se pulse una tecla
     evt = evt || window.event;
     var charCode = evt.keyCode || evt.which;
-    if(charCode==170) {	// Si la tecla pulsada es ª...
-        if(key === null) {	// Si aún no se ha establecido la clave...
-            var globalkey=prompt("PASSWORD OF COMMUNICATION");	// Pedimos la clave de cifrado para la comunicación
-            if(globalkey === "" || globalkey === null) { return; }	// Si no se introduce contraseña, cerramos la función
-            var md5key=hex_md5(globalkey);	// Hasheamos la clave en md5 para obtener siempre una clave de 256 bits
-            key=AESW_init(md5key);	// Variable con la llave
+    if(charCode==170) { // Si la tecla pulsada es ª...
+        if(key === null) {  // Si aún no se ha establecido la clave...
+            var globalkey=prompt("PASSWORD OF COMMUNICATION");  // Pedimos la clave de cifrado para la comunicación
+            if(globalkey === "" || globalkey === null) { return; }  // Si no se introduce contraseña, cerramos la función
+            var md5key=hex_md5(globalkey);  // Hasheamos la clave en md5 para obtener siempre una clave de 256 bits
+            key=AESW_init(md5key);  // Variable con la llave
             // LIMPIEZA DE VARIABLES QUE PUEDEN SER EXPUESTAS (seguridad)
             globalkey=null;
             md5key=null;
+
+            return false;   // Devolvemos "false" para que no se escriba el carácter ª y para que no se cifre un mensaje en blanco
         }
 
-        var oneChanged = false;	// Variable booleana para saber si se ha descifrado al menos un mensaje
-        $('span[dir="ltr"]').each(function() {	// Por cada etiqueta "span" con el campo dir="ltr"...
-            if($(this).html().substr(0, 5)=="AES: ") {	// Si los primeros 4 bytes son "AES: "...
-                var bencoded=$(this).html().substr(5, $(this).html().length);	// Obtenemos lo que queda después de "AES: "
-                var encoded=atob(bencoded);	// Lo decodificamos de BASE64 para obtener los bytes limpios
-                var decoded=AESW_decryptLongString(encoded, key);	// Decodificamos el mensaje
-                $(this).html(decoded);	// Y lo cambiamos en el documento HTML
-                oneChanged = true;	// Cambiamos el valor de la variable booleana para saber que al menos se ha descifrado un mensaje
+        var oneChanged = false; // Variable booleana para saber si se ha descifrado al menos un mensaje
+        $('span[dir="ltr"]').each(function() {  // Por cada etiqueta "span" con el campo dir="ltr"...
+            if($(this).html().substr(0, 5)=="AES: ") {  // Si los primeros 4 bytes son "AES: "...
+                var bencoded=$(this).html().substr(5, $(this).html().length);   // Obtenemos lo que queda después de "AES: "
+                var encoded=atob(bencoded); // Lo decodificamos de BASE64 para obtener los bytes limpios
+                var decoded=AESW_decryptLongString(encoded, key);   // Decodificamos el mensaje
+                $(this).html(decoded);  // Y lo cambiamos en el documento HTML
+                oneChanged = true;  // Cambiamos el valor de la variable booleana para saber que al menos se ha descifrado un mensaje
             }
         });
 
-        if(oneChanged === false) {	// Si no se ha cambiado ningún mensaje (porque se quiera cifrar texto)...
-            var decoded=msginput.text();	// Variable con el mensaje en texto plano
-            var encrypted=AESW_encryptLongString(decoded, key);	// Ciframos el mensaje
-            var bencrypted=btoa(encrypted);	// Convertimos el mensaje cifrado en imprimible cifrándolo en BASE64
-            msginput.text("AES: "+bencrypted);	// Cambiamos el mensaje a enviar por el mensaje cifrado con el prefijo "AES: "
+        if(oneChanged === false) {  // Si no se ha cambiado ningún mensaje (porque se quiera cifrar texto)...
+            var decoded=msginput.text();    // Variable con el mensaje en texto plano
+            var encrypted=AESW_encryptLongString(decoded, key); // Ciframos el mensaje
+            var bencrypted=btoa(encrypted); // Convertimos el mensaje cifrado en imprimible cifrándolo en BASE64
+            msginput.text("AES: "+bencrypted);  // Cambiamos el mensaje a enviar por el mensaje cifrado con el prefijo "AES: "
         }
 
-        return false;	// Devolvemos "false" para que no se escriba el carácter ª
+        return false;   // Devolvemos "false" para que no se escriba el carácter ª
     }
 }
 
 
-function AB64H_setKeypress() {	// Función que establece el evento keypress del campo de entrada del mensaje
+function AB64H_setKeypress() {  // Función que establece el evento keypress del campo de entrada del mensaje
     msginput=$('div[role="textbox"]');
     msginput.keypress(AB64H_keypress);
 }
